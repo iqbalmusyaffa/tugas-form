cf407b9f4a516c20c0e11acce9846f93a28f380d
iqbalmusyaffa <55908032+iqbalmusyaffa@users.noreply.github.com>
2023-05-17 15:22:51 +0700

    add List data Database

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -54,6 +54,43 @@
                         <th>Last Name</th>
                         <th>Email</th>
                         <th>Age</th>
+                        <th>Position</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($employees as $employee)
+                    <tr>
+                        <td>{{ $employee->firstname }}</td>
+                        <td>{{ $employee->lastname }}</td>
+                        <td>{{ $employee->email }}</td>
+                        <td>{{ $employee->age }}</td>
+                        <td>{{ $employee->position_name }}</td>
+                        <td>
+                            <div class="d-flex">
+                                <a href="{{ route('employees.show', ['employee'=>$employee->employee_id]) }}" class="btn btn-outline-dark btn-sm
+                                    me-2"><i class="bi-person-lines-fill"></i></a>
+                                    <a href="{{ route('employees.edit', ['employee'=>$employee->employee_id]) }}" class="btn btn-outline-dark btn-sm
+                                        me-2"><i class="bi-pencil-square"></i></a>
+                                </div>
+                                <form action="{{ route('employees.destroy',['employee' => $employee->employee_id]) }}" method="POST" @csrf @method('delete')
+                                <button type="submit" class="btn btn-outline-dark btn-sm me-2"><i class="bi-trash"></i></button>
+                                </form>
+                            </div>
+                        </div>
+                        </td>
+                    </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            {{--
+            <table class="table table-bordered table-hover table-striped mb-0 bg-white">
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>Age</th>
                         <th></th>
                     </tr>
                 </thead>
@@ -120,6 +157,7 @@
                     </tr>
                 </tbody>
             </table>
+            --}}
         </div>
     </div>
     @vite('resources/js/app.js')
