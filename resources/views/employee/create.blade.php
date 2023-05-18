9f7427ee8bc63027a0abcaac5f74184a1cd639d0
iqbalmusyaffa <55908032+iqbalmusyaffa@users.noreply.github.com>
2023-05-18 20:42:53 +0700

    add Fixed top in navbar

--- resources/views/employee/create.blade.php
+++ resources/views/employee/create.blade.php
@@ -8,7 +8,7 @@
     @vite('resources/sass/app.scss')
 </head>
 <body>
-    <nav class="navbar navbar-expand-md navbar-dark bg-primary">
+    <nav class="navbar fixed-top navbar-expand-md navbar-dark bg-primary">
         <div class="container">
             <a href="{{ route('home') }}" class="navbar-brand mb-0 h1"><i class="bi-hexagon-fill me-2"></i>Data Master</a>
         <button type="button" class="navbar-toggler" data-bs-toggle="collapse"data-bs-target="#navbarSupportedContent">
